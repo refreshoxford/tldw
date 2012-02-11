6d88baa8023ef8d2e270a6b9b8012ccd55788a22
Henry Blyth <henrah@Henry-Blyths-MacBook-Pro.local>
2012-02-11 17:27:57 +0000

    Popup now gets JSON from drupal instance!

--- tldw_pop.js
+++ tldw_pop.js
@@ -1,5 +1,17 @@
 $(document).ready(function() {
   chrome.extension.sendRequest({msg: 'loadData'}, function(response) {
-    $('body').append('<div>foo: '+response.data+'</div>')
+    var i, data, url;
+    data = response.data;
+    for( i=0; i<data.length; i++ )
+    {
+      url = 'http://tldw.services.refreshoxford.co.uk:8080/tldwapi/search?url='+encodeURIComponent(data[i]);
+      jQuery.getJSON(url, function(transcript) {
+        //alert('Title = '+transcript.title);
+        //alert('Title = '+transcript.link);
+        $('body').append('<div>Title = '+transcript.title+'</div>')
+        $('body').append('<div>Link = '+transcript.link+'</div>')
+      });
+      break;
+    }
   });
 })
